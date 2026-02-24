82aa663098b0593920cdc03dd9f90b31f9091699
Lukas <lukas.soderlund.fanta@gmail.com>
2026-02-24 11:51:03 +0100

    RED

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -14,12 +14,18 @@ import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
-    //put constants here to increase readability
-    const emptyPassword = '';
+  
+     test('getPasswordHash ShouldReturnHashedValueForValidPassword', () => {
+    const validPassword = 'abcdefghijkl1m' // >=12 chars and contains number
+    const sut = new Password(validPassword)
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
-    });
+    // expected hash using same algorithm
+    let expectedHash = 7
+    for (let i = 0; i < validPassword.length; i++) {
+      expectedHash = expectedHash * 31 + validPassword.charCodeAt(i)
+    }
 
-    //Add your tests here
+    expect(sut.getPasswordHash()).toBe(expectedHash)
+  })
+
 });
